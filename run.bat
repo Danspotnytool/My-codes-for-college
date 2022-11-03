0d6799c3a76c0b953f7c826a44b06be089ddbc83
Daniel John Baynosa <firedantsm@gmail.com>
2022-11-03 18:32:41 +0800

    don't echo

--- run.bat
+++ run.bat
@@ -3,7 +3,5 @@
 @REM Get the file name from the arguments
 set FILENAME=%1
 
-@echo Running %FILENAME%
-
 @REM gcc FILENAME.c && a.exe
 gcc %FILENAME% && a.exe
--- run.sh
+++ run.sh
@@ -2,8 +2,6 @@
 # Get the file name from the arguments
 file_name=$1
 
-echo "File name: $file_name"
-
 # gcc file_name.c && ./a.out
 gcc $file_name && ./a.out
 echo ""
