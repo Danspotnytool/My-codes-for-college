708213c9cf0b98b1641560e347ef713115fd2da2
Daniel John <63223301+Danspotnytool@users.noreply.github.com>
2022-11-24 08:30:05 +0000

    flags (experimental)

--- run.sh
+++ run.sh
@@ -1,7 +1,36 @@
+#!/bin/sh
 
 # Get the file name from the arguments
 file_name=$1
 
-# gcc file_name.c && ./a.out
-gcc $file_name -lm && ./a.out
+# Get flags all from the arguments
+# --clear: clear the screen before running the program
+# --infinite: run the program infinitely unless the user stops it
+flags=${@:2}
+
+gcc $file_name -lm
+
+if [[ $flags == *"--clear"* ]]; then
+    clear
+fi
+
+# Check if the program should run infinitely
+if [[ $flags == *"--infinite"* ]]; then
+    while [ 1 ]
+    do
+        if [[ $flags == *"--clear"* ]]; then
+            clear
+        fi
+        ./a.out
+
+        # Wait for the user to press enter
+        echo ""
+        echo ""
+        echo ""
+        read -p "Program finished. Press enter to run again."
+    done
+else
+    ./a.out
+fi
+
 echo ""
